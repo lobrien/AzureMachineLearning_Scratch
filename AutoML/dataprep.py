09b94654b01cdbd8b874273faa53fb9e212e00f3
lobrien <lobrien@knowing.net>
2020-09-01 14:01:32 -1000

    Titanic AutoML

---
+++ AutoML/dataprep.py
@@ -0,0 +1,61 @@
+from azureml.core import Run
+
+import pandas as pd 
+import numpy as np 
+import pyarrow as pa
+import pyarrow.parquet as pq
+from sklearn.model_selection import train_test_split
+import argparse
+
+RANDOM_SEED=42
+
+def prepare_age(df):
+    # Fill in missing Age values from distribution of present Age values 
+    mean = df["Age"].mean()
+    std = df["Age"].std()
+    is_null = df["Age"].isnull().sum()
+    # compute enough (== is_null().sum()) random numbers between the mean, std
+    rand_age = np.random.randint(mean - std, mean + std, size = is_null)
+    # fill NaN values in Age column with random values generated
+    age_slice = df["Age"].copy()
+    age_slice[np.isnan(age_slice)] = rand_age
+    df["Age"] = age_slice
+    df["Age"] = df["Age"].astype(int)
+    
+    # Quantize age into 5 classes
+    df['Age_Group'] = pd.qcut(df['Age'],5, labels=False)
+    df.drop(['Age'], axis=1, inplace=True)
+    return df
+
+def prepare_fare(df):
+    df['Fare'].fillna(0, inplace=True)
+    df['Fare_Group'] = pd.qcut(df['Fare'],5,labels=False)
+    df.drop(['Fare'], axis=1, inplace=True)
+    return df 
+
+def prepare_genders(df):
+    genders = {"male": 0, "female": 1, "unknown": 2}
+    df['Sex'] = df['Sex'].map(genders)
+    df['Sex'].fillna(2, inplace=True)
+    df['Sex'] = df['Sex'].astype(int)
+    return df
+
+def prepare_embarked(df):
+    df['Embarked'].replace('', 'U', inplace=True)
+    df['Embarked'].fillna('U', inplace=True)
+    ports = {"S": 0, "C": 1, "Q": 2, "U": 3}
+    df['Embarked'] = df['Embarked'].map(ports)
+    return df
+    
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_path', dest='output_path', required=True)
+args = parser.parse_args()
+    
+titanic_ds = Run.get_context().input_datasets['titanic_ds']
+df = titanic_ds.to_pandas_dataframe().drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+df = prepare_embarked(prepare_genders(prepare_fare(prepare_age(df))))
+
+os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+pq.write_table(pa.Table.from_pandas(df), args.output_path)
+
+print(f"Wrote test to {args.output_path} and train to {args.output_path}")
