e46ecbf2046a93191d3bdbace8055b1d8df1e2cc
lobrien <lobrien@knowing.net>
2020-11-23 13:04:00 -1000

    Working code for how-to-deploy-local.md

---
+++ LocalDeploy/deploy_local_with_docker.py
@@ -0,0 +1,23 @@
+from azureml.core.webservice import LocalWebservice
+from azureml.core.model import InferenceConfig
+from azureml.core.environment import Environment
+from azureml.core import Workspace
+from azureml.core.model import Model
+
+ws = Workspace.from_config()
+model = Model(ws, 'sklearn_mnist')
+
+myenv = Environment.get(workspace=ws, name="local-deploy-env")
+
+inference_config = InferenceConfig(entry_script="score.py", environment=myenv)
+
+deployment_config = LocalWebservice.deploy_configuration(port=6789)
+
+local_service = Model.deploy(workspace=ws, 
+                       name='sklearn-mnist-local-v2', 
+                       models=[model], 
+                       inference_config=inference_config, 
+                       deployment_config = deployment_config)
+
+local_service.wait_for_deployment(show_output=True)
+print(f"Scoring URI is : {local_service.scoring_uri}")
